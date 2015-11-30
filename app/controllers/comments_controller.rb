6fcef11287013c898afec85c6c39f3650fd3d966
Alexis La Porte <aklaporte@gmail.com>
2015-11-30 17:22:48 -0500

    refactor create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,13 @@ class CommentsController < ApplicationController
   def create
     @user = current_user
     @comment = current_user.comments.new(comment_params)
-    if @comment.save
-      if @comment.commentable_type == "Answer"
+
+    if @comment.save && @comment.commentable_type == "Answer"
         redirect_to question_path(@comment.commentable.question_id)
-       else
+    elsif @comment.save && @comment.commentable_type == "Question"
         redirect_to question_path(@comment.commentable_id)
-      end
     else
+      #TODO: Add error handling, change path
       redirect_to root_path
     end
   end
